525d1ec55fa5fc956e8607744a9185d9d784d32f
edcaetanoguedes <ednaldocaetanoguedes@gmail.com>
2025-07-01 18:44:22 -0300

    chore: instalação e configuração das ferramentas de teste

---
+++ cypress.config.js
@@ -0,0 +1,26 @@
+const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
+const { defineConfig } = require("cypress");
+const createEsbuildPlugin = require("@badeball/cypress-cucumber-preprocessor/esbuild").createEsbuildPlugin;
+const addCucumberPreprocessorPlugin = require("@badeball/cypress-cucumber-preprocessor").addCucumberPreprocessorPlugin;
+require("dotenv").config();
+
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: "http://localhost:3000",
+    specPattern: "cypress/e2e/**/**/*.feature",
+    viewportWidth: 1920,
+    viewportHeight: 1080,
+    async setupNodeEvents(on, config) {
+      await addCucumberPreprocessorPlugin(on, config)
+      on("file:preprocessor", createBundler({
+        plugins: [createEsbuildPlugin(config)]
+      }))
+      return config;
+    }
+  },
+  env: {
+    grepFilterSpecs: true,
+    FRONTEND_URL: "http://localhost:3000"
+  }
+})
